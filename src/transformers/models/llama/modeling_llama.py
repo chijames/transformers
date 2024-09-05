d5a0c5cb88d16a69dc05e6805236ffb823af74ab
tachung <tachung.chi@databricks.com>
2024-09-05 18:19:01 +0000

    0.3

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -1166,7 +1166,7 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
             shift_logits = logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
             # Flatten the tokens
-            loss_fct = CrossEntropyLoss(label_smoothing=0.2)
+            loss_fct = CrossEntropyLoss(label_smoothing=0.3)
             shift_logits = shift_logits.view(-1, self.config.vocab_size)
             shift_labels = shift_labels.view(-1)
             # Enable model parallelism
